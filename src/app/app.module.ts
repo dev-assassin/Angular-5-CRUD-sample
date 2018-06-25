19356f753a85c04c09fd18e123dfed7ee06c69d7
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-25 08:33:37 +0800

    add initial comments

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    CreateComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
