072966d6c70073704f8fb809bc54d227f66e3fe0
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-25 21:19:58 +0800

    add model and expressrouter files

---
+++ models/Coin.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Define collection and schema for Items
+var Coin = new Schema({
+    name: {
+        type: String
+    },
+    price: {
+        type: Number
+    }
+}, {
+    collection: 'coins'
+});
