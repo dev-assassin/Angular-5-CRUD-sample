b78e119119a1c2de9e9da58234770e623a3450f2
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-25 09:08:12 +0800

    move components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
+import { IndexComponent } from './components/index/index.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
 
