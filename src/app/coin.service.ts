b64ccb9638e6064beb67ae9aca04612893019e1f
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-26 02:44:57 +0800

    update coin.service.ts

--- src/app/coin.service.ts
+++ src/app/coin.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoinService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  addCoin(name, price) {
+    const uri = 'http://localhost:4200/coins/add';
+    const obj = {
+      name: name,
+      price: price
+    };
+    this.http.post(uri, obj)
+      .subscribe(res => console.log('Done'));
+  }
 }
